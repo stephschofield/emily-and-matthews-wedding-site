d3d4093624c7861ec1046121abae9382408c06a4
Stephanie Schofield <sschofield@microsoft.com>
2026-01-05 01:07:09 -0600

    feat: add Tsunami Sushi restaurant image

--- app/activities/page.tsx
+++ app/activities/page.tsx
@@ -52,6 +52,7 @@ const restaurants: Activity[] = [
     description: "Enjoy Sushi and a view of the Mississippi River at Tsunami Sushi!",
     url: "https://batonrouge.servingsushi.com",
     icon: "utensils",
+    image: "/images/tsunami-sushi.jpg",
   },
   {
     title: "Superior Grill on Highland",
